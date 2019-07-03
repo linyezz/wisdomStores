7d2f2082a994c0fc27c906745803315a8e398deb
linyezz <zhangzheng@zhangzhengdeMacBook-Pro.local>
2019-07-03 19:39:00 +0800

    change code

--- pages/camera/takeVideo/takeVideo.js
+++ pages/camera/takeVideo/takeVideo.js
@@ -5,62 +5,104 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    times: '00',
+    is_record:true,
+    tempThumbPath:'',
+    tempVideoPath:'',
+    record_finish:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    var that = this;
+    try {
+      let system = wx.getSystemInfoSync();
+      let basewidth = system.windowWidth / 750;
+      that.data.basewidth = basewidth;
+      that.ctx = wx.createCanvasContext('canvas', that);
+      that.ctx.draw(false);
+      // that.drawCanvas()
+     
+    } catch (e) {
 
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  recordingStart () {
+    let that=this;
+    let is_record = that.data.is_record;
+    let basewidth = that.data.basewidth;
+    that.ctx.draw();
+    if(is_record){
+      that.setData({
+        is_record:false
+      })
+      this.camera.startRecord({
+        
+      })
+      let i = 0;
+      let circle = setInterval(function(){
+        i++;
+        if(i>60){
+          clearInterval(circle)
+          that.getVideo()
+          return;
+          
+        }
+        var time = '';
+          var s = parseInt(i / 20);
+          var min = Math.floor(s / 60) % 60;
+          var sec = s % 60;
+          if (min < 10) { time += "0"; }
+          time += min + ":";
+          if (sec < 10) { time += "0"; }
+          time += sec;
+          // console.log(time)
+          that.setData({
+            times:time
+          })
+        that.ctx.setStrokeStyle('#EC6B7B');
+        that.ctx.setLineWidth(8 * basewidth)
+        that.ctx.beginPath();
+        that.ctx.arc(750 * basewidth / 2, 85 * basewidth, 80* basewidth, 0, Math.PI * (2 / 60) * i  );
+        that.ctx.stroke();
+        that.ctx.draw(false);
+        that.data.circle = circle;
+      },50)
+      
+      
+      
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  //相机加载完成
+  loadCamera(){
+    const context = wx.createCameraContext();
+    this.camera = context;
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  
+  getVideo(){
+    let that=this;
+    this.camera.stopRecord({
+      success(res) {
+       that.setData({
+         tempThumbPath:res.tempThumbPath,
+         tempVideoPath:res.tempVideoPath,
+         record_finish:true
+       }) 
+       
+      },
+      fail() {
+        console.log('失败')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  recordingAgain(){
+    var that=this;
+    that.setData({
+      is_record:true,
+      record_finish:false
+    }) 
   }
+ 
 })
